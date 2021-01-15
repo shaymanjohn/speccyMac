0b29f0710db4566d9a6d2c1cbd57a4b951c75412
John Ward <johnmartinward@gmail.com>
2021-01-15 11:12:56 +0000

    jenkins

--- BuildScripts/jenkins-build.sh
+++ BuildScripts/jenkins-build.sh
@@ -9,6 +9,6 @@ BUILD_SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$BUILD_SCRIPTS_DIR/.."
 
 echo "\n=== Building speccyMac ===\n"
-set -o pipefail && xcodebuild -scheme $SCHEME -configuration Debug build test -destination "platform=macOS,arch=x86_64" | /usr/local/bin/xcpretty -r junit
+set -o pipefail && xcodebuild -scheme $SCHEME -configuration Debug build test -destination "platform=macOS,arch=x86_64" #| /usr/local/bin/xcpretty -r junit
 
 cd - >> /dev/null
